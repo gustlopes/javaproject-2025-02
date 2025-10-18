d8d629bf29babee77adc5408c87900a233ea5123
Gustavo Lopes <gustkng@gmail.com>
2025-10-17 22:40:46 -0300

    Correção 2.0
    Mais um erro bobo de sintaxe. Agora está corrigido de fato.

---
+++ product-service/src/main/resources/db_migration/V2__PopulateTableTbProduct.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_product (description, brand, model, currency, price, stock) VALUES
+('PlayStation 5', 'Sony', 'PS5 825GB', 'USD', 499.99, 15),
+('PlayStation 2', 'Sony', 'PS2 Slim 32MB', 'USD', 199.99, 5),
+('PC Positivo', 'Positivo', 'Roda joguinho e trava na calculadora', 'USD', 349.99, 20),
+('PC Gamer RGB', 'Custom', 'Roda tudo, menos Crysis', 'USD', 1599.99, 8);
